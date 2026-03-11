3b4f04d2121b39bb19ecf124f7b188cdce19ba8b
Karthika Karumudi <karthikakarumudi@gmail.com>
2026-03-11 17:14:36 +1100

    Fix CORS to allow any origin

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -3,47 +3,37 @@ using Backend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add controllers with JSON camelCase serialization
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
     });
 
-// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// SQLite Database
 builder.Services.AddDbContext<ChronicDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")
         ?? "Data Source=chronic.db"));
 
-// CORS - allow React frontend on any localhost port
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins(
-                "http://localhost:3000",
-                "http://localhost:3001",
-                "http://localhost:5173"
-            )
-            .AllowAnyHeader()
-            .AllowAnyMethod();
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
     });
 });
 
 var app = builder.Build();
 
-// Auto-create/migrate database on startup
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ChronicDbContext>();
     db.Database.EnsureCreated();
 }
 
-// Swagger UI in development
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
